093520186f52952230bc845ec8f80d6d90f7d82a
Yeikel Uriarte Arteaga <uyeikel@gmail.com>
2023-03-26 17:35:00 -0400

    update

--- native_lib/tools/build_ios.sh
+++ native_lib/tools/build_ios.sh
@@ -14,4 +14,4 @@ cd build/ios
 cmake ../.. -DCMAKE_TOOLCHAIN_FILE=../../ios-cmake/toolchain/iOS.cmake -DIOS_PLATFORM=SIMULATOR64 -GXcode
 
 # Build the project using xcodebuild
-xcodebuild build -project whisper_library.xcodeproj -configuration Release -scheme whisper_library -destination 'platform=iOS Simulator,name=iPhone 11'
+xcodebuild build -project whisper_library.xcodeproj -configuration Release -scheme whisper_library -destination 'platform=iOS'
